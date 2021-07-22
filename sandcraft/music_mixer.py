3c51aa2ad8299cf0a8b3972712d9704ec21fa91f
anthonyvkane <62270439+anthonyvkane@users.noreply.github.com>
2021-07-22 08:06:10 -0400

    Update music_mixer.py

--- sandcraft/music_mixer.py
+++ sandcraft/music_mixer.py
@@ -4,10 +4,13 @@ class MusicMixer:
     def __init__(self):
         mixer.init()
         self.__foundParticle = mixer.Sound("music/newParticle.wav")
+        self.__foundParticle.set_volume(.05)
         #self.__backgroundMusic = mixer.Sound("music/background.wav")
+        #self.__backgroundMusic.set_volume(.05)
+        #self.backgroundPlay()
     
     def ding_Discovery(self):
         self.__foundParticle.play()
 
     #def backgroundPlay(self):
-    #    self.__backgroundMusic.play()
+    #    self.__backgroundMusic.play(-1)
