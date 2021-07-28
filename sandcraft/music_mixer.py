ff448244bec2a9caf75a2dc9203be4d9aa06d6a7
anthonyvkane <62270439+anthonyvkane@users.noreply.github.com>
2021-07-27 22:13:46 -0400

    Update music_mixer.py

--- sandcraft/music_mixer.py
+++ sandcraft/music_mixer.py
@@ -1,18 +1,26 @@
 import os
 from pygame import mixer
 
-
 class MusicMixer:
     def __init__(self):
         mixer.init()
         self.__foundParticle = mixer.Sound(os.path.join(os.path.dirname(__file__), 'soundFiles', 'newParticle.wav'))
         self.__foundParticle.set_volume(.05)
-        # self.__backgroundMusic = mixer.Sound("music/background.wav")
-        # self.__backgroundMusic.set_volume(.05)
-        # self.backgroundPlay()
-
+        self.__backgroundMusic = mixer.Sound(os.path.join(os.path.dirname(__file__), 'soundFiles', 'background.wav'))
+        self.__backgroundMusic.set_volume(.05)
+        self.backgroundPlay()   
+        print("created musicmixer..")
+    
     def ding_Discovery(self):
         self.__foundParticle.play()
 
-    # def backgroundPlay(self):
-    #    self.__backgroundMusic.play(-1)
+    def backgroundPlay(self):
+        self.__backgroundMusic.play(-1)
+
+    def change_volume_background(self, vol):
+        self.__backgroundMusic.set_volume(vol)
+
+    def endMixer(self):
+        mixer.fadeout(1500)
+        print("deleting music mixer..")
+        del self
