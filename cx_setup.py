2d9544a186255d082ef88ca6ca4f88b99a672d50
anthonyvkane <62270439+anthonyvkane@users.noreply.github.com>
2021-07-29 01:20:13 -0400

    Update cx_setup.py

--- cx_setup.py
+++ cx_setup.py
@@ -1,9 +1,28 @@
 import sys
-from cx_Freeze import setup, Executable
+from cx_Freeze import *
 
 build_exe_opts = {  
-                    "packages": ["pygame","sandcraft"]
+                    "packages": ["pygame","sandcraft"],
                 }
+
+shortcut_table = [
+    ("DesktopShortcut",        # Shortcut
+     "DesktopFolder",          # Directory_
+     "sandcraft",           # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]sandcraft.exe",# Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     )
+    ]
+
+msi_data = {"Shortcut": shortcut_table}
+bdist_msi_options = {'data': msi_data}
 
 base = None
 if sys.platform == "win32":
@@ -14,6 +33,8 @@ setup(
     version = "1.0",
     author = "jsessa@ufl.edu",
     description = "Particle physics sandbox game",
-    options = {"build_exe": build_exe_opts},
+    options = { "build_exe" : build_exe_opts,
+                "bdist_msi" : bdist_msi_options
+                },
     executables = [Executable("sandcraft/main.py", base = base, target_name = "sandcraft.exe")]
 )
